8c523463216f935f87916857b7f43462f86405a6
David Sheinberg <David_Sheinberg@brown.edu>
2026-01-07 16:15:25 -0500

    Add tdom 0.9.6 with pre-built binaries

--- vfs/lib/tdom/pkgIndex.tcl
+++ vfs/lib/tdom/pkgIndex.tcl
@@ -3,10 +3,10 @@
 #
 
 if { $::tcl_platform(os) == "Windows NT" } {
-    package ifneeded tdom 0.9.5 \
-	[list load [file join $dir $::tcl_platform(os) $::tcl_platform(machine) tcl9tdom095[info sharedlibextension]]]
+    package ifneeded tdom 0.9.6 \
+	[list load [file join $dir $::tcl_platform(os) $::tcl_platform(machine) tcl9tdom096[info sharedlibextension]]]
 } else {
-    package ifneeded tdom 0.9.5 \
-	[list load [file join $dir $::tcl_platform(os) $::tcl_platform(machine) libtcl9tdom0.9.5[info sharedlibextension]] Tdom]
+    package ifneeded tdom 0.9.6 \
+	[list load [file join $dir $::tcl_platform(os) $::tcl_platform(machine) libtcl9tdom0.9.6[info sharedlibextension]] Tdom]
 }
 
